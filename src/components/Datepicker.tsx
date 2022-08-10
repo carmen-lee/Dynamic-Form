5215ba10463f8749eb608b6e7f393bd6d5b8cc82
Carmen Lee <carmen.lee.lv@gmail.com>
2022-08-09 17:08:35 -0700

    added Datepicker component

---
+++ src/components/Datepicker.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import { makeStyles } from "@material-ui/core";
+
+const Datepicker: React.FC<DatepickerProps> = ({
+    useStyles = makeStyles({}),
+    fieldKey,
+    fieldMandatory,
+    fieldPrompt,
+    handleChange
+}: DatepickerProps): ReactElement => {
+    const classes = useStyles();
+    return (
+        <div>
+            <label>{fieldPrompt}</label>
+            <input type="date"></input>
+        </div>
+    )
+}
+
+interface DatepickerProps {
+    useStyles?: any;
+    fieldKey: string;
+    fieldMandatory: string;
+    fieldPrompt: string;
+    handleChange: (e: React.ChangeEvent<HTMLSelectElement>, id: string, value:any) => void;
+}
+
+interface OptionList {
+    optionLabel: string;
+}
+
+export default Datepicker
